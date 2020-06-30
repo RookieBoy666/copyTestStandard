33095a15c3c1f047dd991c56768a429f545a5f2b
zjp <2441494040@qq.com>
2020-06-30 17:12:32 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace copyTestStandard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.old = new System.Windows.Forms.TextBox();
+            this.newTest = new System.Windows.Forms.TextBox();
+            this.sCreator = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(171, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "复制";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // old
+            // 
+            this.old.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.old.Location = new System.Drawing.Point(171, 44);
+            this.old.Name = "old";
+            this.old.Size = new System.Drawing.Size(165, 30);
+            this.old.TabIndex = 1;
+            // 
+            // newTest
+            // 
+            this.newTest.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+            this.newTest.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.newTest.Location = new System.Drawing.Point(171, 80);
+            this.newTest.Name = "newTest";
+            this.newTest.Size = new System.Drawing.Size(165, 30);
+            this.newTest.TabIndex = 2;
+            // 
+            // sCreator
+            // 
+            this.sCreator.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.sCreator.Location = new System.Drawing.Point(171, 116);
+            this.sCreator.Name = "sCreator";
+            this.sCreator.Size = new System.Drawing.Size(165, 30);
+            this.sCreator.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "原测试名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "新测试名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "创建人";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 269);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sCreator);
+            this.Controls.Add(this.newTest);
+            this.Controls.Add(this.old);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "测试标准复制";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox old;
+        private System.Windows.Forms.TextBox newTest;
+        private System.Windows.Forms.TextBox sCreator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace copyTestStandard
+{
+    public partial class Form1 : Form
+    {
+        public string connStr = "Data Source=192.168.88.6;Initial Catalog = HSFabricTrade_HZFE; Persist Security Info = True;User ID = zjp; Password =123456 ;Connect Timeout = 8";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrEmpty(old.Text))
+                {
+                MessageBox.Show("原测试名不能为空！");
+                return;
+
+            }
+            if (string.IsNullOrEmpty(newTest.Text))
+                {
+                MessageBox.Show("新测试名不能为空！");
+                return;
+            }
+            if (string.IsNullOrEmpty(sCreator.Text))
+                {
+                MessageBox.Show("创建人不能为空！");
+                return;
+            }
+            try
+            {
+                string sql = "EXEC dbo.copyTestStandard '" + old.Text.Trim().ToString() + "'," + "'" + newTest.Text.Trim().ToString() + "','" + sCreator.Text.Trim().ToString() + "'";
+                SqlConnection Sqlconn = new SqlConnection(connStr);
+                SqlCommand cmd = new SqlCommand(sql, Sqlconn);
+                // SqlCommand getsModelcmd=new SqlCommand(getsModel, Sqlconn);
+                Sqlconn.Open();
+
+                cmd.ExecuteNonQuery();//更新qmRawInspectHdr的重量
+                Sqlconn.Close();
+                MessageBox.Show("复制成功！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "复制失败！");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            sCreator.Text = "王丽";
+        }
+    }
+}
